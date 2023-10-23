5d275ff80a0720e2cfa4ab11888f9ec1004ddbd3
vatszal <135651028+vatszal@users.noreply.github.com>
2023-10-23 23:45:40 +0530

    dark mode added

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 let mode = document.querySelector(".colormode")
-mode.onclick = function(){document.body.classList.toggle("darkmode")}
+mode.onclick = function () {
+    document.body.classList.toggle("darkmode");
+    let icon = document.querySelector(".icon");
+    let headericon = document.querySelector(".header img");
+    let decorative = document.querySelector(".main img")
+    if (document.body.classList.contains("darkmode")) {
+        icon.classList.add("ri-sun-line");
+        icon.classList.remove("ri-contrast-2-fill");
+        headericon.src = "img/logo black.png";
+        decorative.src="img/moon.gif";
+    }
+    else {
+        icon.classList.add("ri-contrast-2-fill");
+        icon.classList.remove("ri-sun-line");
+        headericon.src = "img/logo white.png";
+        decorative.src="img/sun.gif";
+    }
+}
+
