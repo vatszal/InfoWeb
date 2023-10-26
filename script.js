6604b50f5fd18d3d12eed9bd6236f85caa92f9c0
vatszal <135651028+vatszal@users.noreply.github.com>
2023-10-26 19:42:53 +0530

    sunset image added

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ mode.onclick = function () {
     let icon = document.querySelector(".icon");
     let headericon = document.querySelector(".header img");
     let decorative = document.querySelector(".main img")
+    let cloud = document.querySelector(".main");
     if (document.body.classList.contains("darkmode")) {
         icon.classList.add("ri-sun-line");
         icon.classList.remove("ri-contrast-2-fill");
         headericon.src = "img/logo black.png";
-        decorative.src="img/moon.gif";
+        decorative.src = 'img/sunset.png';
+        cloud.style.backgroundImage = "url('img/sunset2.jpg')";
+
     }
     else {
         icon.classList.add("ri-contrast-2-fill");
         icon.classList.remove("ri-sun-line");
         headericon.src = "img/logo white.png";
         decorative.src="img/sun.gif";
+        cloud.style.backgroundImage = "url('img/clouds.png')";
     }
 }
 
