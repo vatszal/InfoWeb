f97ed3c9b558c19a158f431235e5140332ed7832
vatszal <135651028+vatszal@users.noreply.github.com>
2023-11-10 21:28:42 +0530

    added dark mode retain

--- script.js
+++ script.js
@@ -1,24 +1,69 @@
-let mode = document.querySelector(".colormode")
-mode.onclick = function () {
-    document.body.classList.toggle("darkmode");
+document.addEventListener("DOMContentLoaded", function () {
+    let body = document.body;
     let icon = document.querySelector(".icon");
     let headericon = document.querySelector(".header img");
-    let decorative = document.querySelector(".main img")
+    let decorative = document.querySelector(".main img");
     let cloud = document.querySelector(".main");
-    if (document.body.classList.contains("darkmode")) {
+
+    // Check for dark mode on page load and apply styles
+    checkDarkModeOnLoad();
+
+    // Check screen width on page load and enable dark mode if below 600px
+    if (window.innerWidth < 600) {
+        body.classList.add("darkmode");
+        enableDarkMode();
+        // Store the dark mode state in localStorage
+        localStorage.setItem("darkmode", "enabled");
+    }
+
+    let mode = document.querySelector(".colormode");
+    mode.addEventListener("click", function () {
+        body.classList.toggle("darkmode");
+
+        if (body.classList.contains("darkmode")) {
+            enableDarkMode();
+            // Store the dark mode state in localStorage
+            localStorage.setItem("darkmode", "enabled");
+        } else {
+            disableDarkMode();
+            // Remove the dark mode state from localStorage
+            localStorage.removeItem("darkmode");
+        }
+    });
+
+    function enableDarkMode() {
         icon.classList.add("ri-sun-line");
         icon.classList.remove("ri-contrast-2-fill");
-        headericon.src = "img/logo black.png";
-        decorative.src = 'img/sunset.png';
-        cloud.style.backgroundImage = "url('img/sunset2.jpg')";
-
+        if (headericon) {
+            headericon.src = "img/logo black.png";
+        }
+        if (decorative) {
+            decorative.src = 'img/sunset.png';
+        }
+        if (cloud) {
+            cloud.style.backgroundImage = "url('img/sunset2.jpg')";
+        }
     }
-    else {
+
+    function disableDarkMode() {
         icon.classList.add("ri-contrast-2-fill");
         icon.classList.remove("ri-sun-line");
-        headericon.src = "img/logo white.png";
-        decorative.src="img/sun.gif";
-        cloud.style.backgroundImage = "url('img/clouds.png')";
+        if (headericon) {
+            headericon.src = "img/logo white.png";
+        }
+        if (decorative) {
+            decorative.src = "img/sun.gif";
+        }
+        if (cloud) {
+            cloud.style.backgroundImage = "url('img/clouds.png')";
+        }
     }
-}
 
+    function checkDarkModeOnLoad() {
+        // Check if dark mode is enabled in localStorage
+        if (localStorage.getItem("darkmode") === "enabled") {
+            body.classList.add("darkmode");
+            enableDarkMode();
+        }
+    }
+});
